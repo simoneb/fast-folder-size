c246085a9d33942a23c02f7e85d7c0684e2f2f45
Simone Busoli <simone.busoli@gmail.com>
2021-03-09 16:04:30 +0100

    docs: readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # fast-folder-size
+
+Node CLI or module to calculate folder size.
+
+It uses:
+
+- [Sysinternals DU](https://docs.microsoft.com/en-us/sysinternals/downloads/du) on Windows, automatically downloaded at installation time because the license does not allow redistribution
+- native `du` on other platforms
+
+## Installation
+
+```
+npm i fast-folder-size
+```
+
+## Usage
+
+### Programmatically
+
+```js
+const { promisify } = require('util')
+const fastFolderSize = require('fast-folder-size')
+
+// callback
+fastFolderSize('.', (err, bytes) => {
+  if (err) {
+    throw err
+  }
+
+  console.log(bytes)
+})
+
+// promise
+const fastFolderSizeAsync = promisify(fastFolderSize)
+const bytes = await fastFolderSizeAsync('.')
+
+console.log(bytes)
+```
+
+### Command line
+
+```bash
+fast-folder-size .
+```
--- index.js
+++ index.js
@@ -4,48 +4,45 @@ const path = require('path')
 const { exec } = require('child_process')
 
 function fastFolderSize(target, cb) {
+  // windows
   if (process.platform === 'win32') {
-    exec(
+    return exec(
       `${path.join(
         __dirname,
         'bin',
         'du.exe'
       )} -nobanner -accepteula ${target}`,
       (err, stdout) => {
-        if (err) {
-          return cb(err)
-        }
+        if (err) return cb(err)
 
         const match = /Size:\s+(.+) bytes/.exec(stdout)
-
         const bytes = match[1].replace(/\./g, '')
 
         cb(null, bytes)
       }
     )
-  } else {
-    exec(`du -s ${target}`, (err, stdout) => {
-      if (err) {
-        return cb(err)
-      }
-
-      const match = /^(\d+)/.exec(stdout)
-
-      const bytes = Number(match[1]) * 1024
-
-      cb(null, bytes)
-    })
   }
+
+  // other platforms
+  exec(`du -s ${target}`, (err, stdout) => {
+    if (err) return cb(err)
+
+    const match = /^(\d+)/.exec(stdout)
+
+    const bytes = Number(match[1]) * 1024
+
+    cb(null, bytes)
+  })
 }
 
 module.exports = fastFolderSize
 
 if (require.main === module) {
-  fastFolderSize(process.argv.slice(2)[0], (err, result) => {
+  fastFolderSize(process.argv.slice(2)[0], (err, bytes) => {
     if (err) {
       throw err
     }
 
-    console.log(result)
+    console.log(bytes)
   })
 }
