84103e23c0b0185e8559fabeca1d15306b52730f
Marco Fugaro <marco.fugaro@gmail.com>
2021-11-29 18:45:07 +0100

    Force execSync to output string

--- sync.js
+++ sync.js
@@ -5,7 +5,7 @@ const { commands, processOutput } = require('./os.js')
 
 function fastFolderSize(target) {
   const command = commands[process.platform] || commands['linux']
-  const stdout = execSync(command, { cwd: target })
+  const stdout = execSync(command, { cwd: target }).toString()
 
   const processFn = processOutput[process.platform] || processOutput['linux']
   const bytes = processFn(stdout)
