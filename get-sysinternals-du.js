f0ba6225f736e4120bb4f9783edb3e3621250504
markmclauchlan <mark.mclauchlan@gmail.com>
2022-05-30 10:24:42 +0100

    Update get-sysinternals-du.js
    Co-authored-by: Simone Busoli <simone.busoli@gmail.com>

--- get-sysinternals-du.js
+++ get-sysinternals-du.js
@@ -7,7 +7,7 @@ if (process.platform !== 'win32') {
   process.exit(0)
 }
 
-const duZipLocation = process.env.FAST_FOLDER_SIZE_DU_ZIP_LOCATION ?? 'https://download.sysinternals.com/files/DU.zip';
+const duZipLocation = process.env.FAST_FOLDER_SIZE_DU_ZIP_LOCATION || 'https://download.sysinternals.com/files/DU.zip';
 
 https.get(duZipLocation, function (res) {
   res.pipe(unzipper.Extract({ path: path.join(__dirname, 'bin') }))
