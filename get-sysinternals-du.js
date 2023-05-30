d741c2da344a6380fcb7cd402a27588e8de6a4e3
Muhammad Ubaid Raza <mubaidr@gmail.com>
2023-05-30 04:33:30 +0000

    add proxy support

--- get-sysinternals-du.js
+++ get-sysinternals-du.js
@@ -11,6 +11,23 @@ const duZipLocation =
   process.env.FAST_FOLDER_SIZE_DU_ZIP_LOCATION ||
   'https://download.sysinternals.com/files/DU.zip'
 
-https.get(duZipLocation, function (res) {
-  res.pipe(unzipper.Extract({ path: path.join(__dirname, 'bin') }))
-})
+// checks for proxy variables in user environment
+const proxy = process.env.HTTPS_PROXY || process.env.https_proxy || process.env.HTTP_PROXY || process.env.http_proxy
+
+if (proxy) {
+  const options = new URL(proxy);
+  options.rejectUnauthorized = false
+
+  https.get({
+    path: duZipLocation,    
+    agent: new https.Agent(options)
+  }, (res) => {
+    res.pipe(unzipper.Extract({ path: path.join(__dirname, 'bin') }))
+  })
+} else {
+  https.get({
+    path: duZipLocation
+  }, function (res) {
+    res.pipe(unzipper.Extract({ path: path.join(__dirname, 'bin') }))
+  })
+}
