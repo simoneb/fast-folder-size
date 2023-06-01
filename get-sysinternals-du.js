cc13b04e71dc7d62a638359a8b31e3baddf500d6
Muhammad Ubaid Raza <mubaidr@gmail.com>
2023-06-01 13:00:35 +0500

    [Improvement] DU download (#25)
    * add proxy support
    
    * cleanup
    
    * chekc if du already exists
    
    * use global agent
    
    * lint

--- get-sysinternals-du.js
+++ get-sysinternals-du.js
@@ -9,9 +9,39 @@ if (process.platform !== 'win32') {
   process.exit(0)
 }
 
+// check if du is already installed
+if (
+  fs.existsSync(
+    path.join(__dirname, 'bin', `du${process.arch === 'x64' ? '64' : ''}.exe`)
+  )
+) {
+  process.exit(0)
+}
+
 const duZipLocation =
   process.env.FAST_FOLDER_SIZE_DU_ZIP_LOCATION ||
   'https://download.sysinternals.com/files/DU.zip'
+
+// checks for proxy variables in user environment
+const proxyAddress =
+  process.env.HTTPS_PROXY ||
+  process.env.https_proxy ||
+  process.env.HTTP_PROXY ||
+  process.env.http_proxy
+
+if (proxyAddress) {
+  const proxyOptions = new URL(proxyAddress)
+  const agent = new https.Agent({
+    proxy: {
+      host: proxyOptions.hostname,
+      port: proxyOptions.port || 8080,
+      protocol: proxyOptions.protocol,
+      auth: proxyOptions.auth,
+    },
+  })
+
+  https.globalAgent = agent
+}
 
 https.get(duZipLocation, function (res) {
   const tempFilePath = path.join(os.tmpdir(), 'du.zip')
