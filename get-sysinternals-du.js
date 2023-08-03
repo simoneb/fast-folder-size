794083b67ca9cfc84f6280c5baf87b5b89ea9117
DevDengChao <2325690622@qq.com>
2023-08-03 14:31:01 +0800

    refactor: make get-sysinternals-du.js testable

--- get-sysinternals-du.js
+++ get-sysinternals-du.js
@@ -5,45 +5,52 @@ const path = require('path')
 const decompress = require('decompress')
 const { HttpsProxyAgent } = require('https-proxy-agent')
 
-// Only run for Windows
-if (process.platform !== 'win32') {
-  process.exit(0)
-}
+exports.default = function () {
+  // Only run for Windows
+  if (process.platform !== 'win32') {
+    process.exit(0)
+  }
 
-// check if du is already installed
-if (
-  fs.existsSync(
-    path.join(__dirname, 'bin', `du${process.arch === 'x64' ? '64' : ''}.exe`)
-  )
-) {
-  process.exit(0)
-}
+  // check if du is already installed
+  if (
+    fs.existsSync(
+      path.join(__dirname, 'bin', `du${process.arch === 'x64' ? '64' : ''}.exe`)
+    )
+  ) {
+    process.exit(0)
+  }
 
-const duZipLocation =
-  process.env.FAST_FOLDER_SIZE_DU_ZIP_LOCATION ||
-  'https://download.sysinternals.com/files/DU.zip'
+  const duZipLocation =
+    process.env.FAST_FOLDER_SIZE_DU_ZIP_LOCATION ||
+    'https://download.sysinternals.com/files/DU.zip'
 
-// checks for proxy variables in user environment
-const proxyAddress =
-  process.env.HTTPS_PROXY ||
-  process.env.https_proxy ||
-  process.env.HTTP_PROXY ||
-  process.env.http_proxy
+  // checks for proxy variables in user environment
+  const proxyAddress =
+    process.env.HTTPS_PROXY ||
+    process.env.https_proxy ||
+    process.env.HTTP_PROXY ||
+    process.env.http_proxy
 
-if (proxyAddress) {
-  const agent = new HttpsProxyAgent(proxyAddress)
+  if (proxyAddress) {
+    const agent = new HttpsProxyAgent(proxyAddress)
 
-  https.globalAgent = agent
-}
+    https.globalAgent = agent
+  }
 
-https.get(duZipLocation, function (res) {
-  const tempFilePath = path.join(os.tmpdir(), 'du.zip')
+  https.get(duZipLocation, function (res) {
+    const tempFilePath = path.join(os.tmpdir(), 'du.zip')
 
-  const fileStream = fs.createWriteStream(tempFilePath)
-  res.pipe(fileStream)
+    const fileStream = fs.createWriteStream(tempFilePath)
+    res.pipe(fileStream)
 
-  fileStream.on('finish', function () {
-    fileStream.close()
-    decompress(tempFilePath, path.join(__dirname, 'bin'))
+    fileStream.on('finish', function () {
+      fileStream.close()
+      decompress(tempFilePath, path.join(__dirname, 'bin'))
+    })
   })
-})
+}
+
+// only auto execute default() function when its invoked directly
+if (require.main === module) {
+  exports.default()
+}
