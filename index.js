aaecbcda02605f48cfd1bc1f047863ecaed2d996
Simone Busoli <simone.busoli@gmail.com>
2025-01-25 10:57:02 +0100

    feat: abort (#149)

--- index.js
+++ index.js
@@ -3,17 +3,27 @@
 const { exec } = require('child_process')
 const { commands, processOutput } = require('./os.js')
 
-function fastFolderSize(target, cb) {
+function fastFolderSize(target, options, cb) {
+  if (typeof options === 'function') {
+    cb = options
+    options = {}
+  }
+
   const command = commands[process.platform] || commands['linux']
 
-  return exec(command, { cwd: target }, (err, stdout) => {
-    if (err) return cb(err)
+  return exec(
+    command,
+    { cwd: target, signal: options.signal },
+    (err, stdout) => {
+      if (err) return cb(err)
 
-    const processFn = processOutput[process.platform] || processOutput['linux']
-    const bytes = processFn(stdout)
+      const processFn =
+        processOutput[process.platform] || processOutput['linux']
+      const bytes = processFn(stdout)
 
-    cb(null, bytes)
-  })
+      cb(null, bytes)
+    }
+  )
 }
 
 module.exports = fastFolderSize
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,13 @@ import { ExecException, ChildProcess } from 'child_process'
 
 declare function fastFolderSize(
   path: string,
+  options: { signal: AbortSignal },
   callback: (err: ExecException | null, bytes?: number) => void
 ): ChildProcess
 
-export = fastFolderSize
+declare function fastFolderSize(
+  path: string,
+  callback: (err: ExecException | null, bytes?: number) => void
+): ChildProcess
+
+export default fastFolderSize
